dd7a2c67bd1dc717998c52b6f089b100c6c65fc8
Gaurav Galiyawala <galiyawalagaurav@gmail.com>
2023-09-26 17:45:38 +0530

    Now you can add comments and post threads show replies etc.

---
+++ components/forms/Comment.tsx
@@ -0,0 +1,88 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "../ui/input";
+import { usePathname, useRouter } from "next/navigation";
+import { CommentValidation } from "@/lib/validations/thread";
+import Image from "next/image";
+import { addCommentToThread } from "@/lib/actions/thread.actions";
+
+interface Props {
+  threadId: string;
+  currentUserImg: string;
+  currentUserId: string;
+}
+const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: "",
+    },
+  });
+  const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
+    try {
+      await addCommentToThread(
+        threadId,
+        values.thread,
+        JSON.parse(currentUserId),
+        pathname
+      );
+      form.reset();
+    } catch (error: any) {
+      throw new Error(`Error creating thread: ${error.message}}`);
+    }
+  };
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="comment-form">
+          <FormField
+            control={form.control}
+            name="thread"
+            render={({ field }) => (
+              <FormItem className="flex items-center gap-3 w-full">
+                <FormLabel>
+                  <div className="relative h-11 w-11">
+                    <Image
+                      src={currentUserImg}
+                      alt="Profile image"
+                      fill
+                      className="cursor-pointer rounded-full object-contain"
+                    />
+                  </div>
+                </FormLabel>
+                <FormControl className="border-none bg-transparent">
+                  <Input
+                    type="text"
+                    placeholder="Comment..."
+                    className="no-focus text-light-1 outline-none"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="comment-form_btn">
+            Reply
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default Comment;
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -38,3 +38,107 @@ export async function createThread({
     console.log(error);
   }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  // Calculate the number of posts to skip based on the page number and page size.
+  const skipAmount = (pageNumber - 1) * pageSize;
+  // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({
+      path: "author",
+      model: User,
+    })
+    .populate({
+      path: "children", // Populate the children field
+      populate: {
+        path: "author", // Populate the author field within children
+        model: User,
+        select: "_id name parentId image", // Select only _id and username fields of the author
+      },
+    });
+
+  // Count the total number of top-level posts (threads) i.e., threads that are not comments.
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  }); // Get the total count of posts
+  const posts = await postsQuery.exec();
+  console.log("🚀 ~ file: thread.actions.ts:70 ~ fetchPosts ~ posts:", posts);
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+  return { posts, isNext };
+}
+
+export async function fetchThreadById(id: string) {
+  connectToDB();
+  try {
+    //TODO: Populate the community field
+    const thread = await Thread.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          {
+            path: "author",
+            model: User,
+            select: "_id id name parentId image",
+          },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (error: any) {
+    throw new Error(`Error fetching thread: ${error.message}`);
+    console.log(error.message);
+  }
+}
+
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) {
+  connectToDB();
+  try {
+    //finding original thread
+    const originalThread = await Thread.findById(threadId);
+    if (!originalThread) throw new Error("Thread not found");
+
+    //create new thread with comment text
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId,
+    });
+
+    //save new thread
+    const saveCommentThread = await commentThread.save();
+
+    //update original thread with new thread
+    originalThread.children.push(saveCommentThread._id);
+
+    await originalThread.save();
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error adding comment to thread: ${error.message}`);
+  }
+}
